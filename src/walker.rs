2adfdfbe0763f3fdac4c4ae70f5e7c9e206b10ca
kubetz <kubetzzz@gmail.com>
2023-04-08 03:00:14 +0200

    feat: add error handling for walker
    - also add more comments
    - switch from Path to PathBuf

--- src/walker.rs
+++ src/walker.rs
@@ -1,17 +1,19 @@
-use std::{fs, io, path::Path};
+use std::{fs::read_dir, path::PathBuf};
 
+use crate::prelude::*;
 use crate::show::Show;
 
-pub fn walker<F>(path: &Path, callback: &F) -> io::Result<()>
-where
-    F: Fn(Show),
-{
-    // First collect all the directories and files.
+/// Walk through the directory recursively to parse all the shows matching directory
+/// names. For each directory representing a show an episode with highest season and
+/// episode number is found and then the callback is called with the parsed [`Show`].
+pub fn walker<F: Fn(Show)>(path: PathBuf, callback: &F) -> Result<()> {
+    // Collect all the directories and files.
     let mut dirs = vec![];
     let mut files = vec![];
 
-    for entry in fs::read_dir(path)? {
-        let entry = entry?;
+    // Read the directory and filter all non-valid entries.
+    for entry in read_dir(path.clone())?.filter_map(|e| e.ok()) {
+        // We will save all the files and directories for for later.
         match entry.file_type()? {
             t if t.is_dir() => dirs.push(entry.path()),
             t if t.is_file() => files.push(entry.path()),
@@ -20,22 +22,21 @@ where
     }
 
     // Try to create a new show from the current directory.
-    // If it is possible, we will start processing files to update the show.
-    if let Some(mut show) = Show::new(path) {
-        // Update the show with the files we found.
-        for file in files {
-            show.update(&file);
-        }
+    let mut show = Show::new(path)?;
 
-        // The show should be valid now. Time for a callback.
-        if show.is_valid() {
-            callback(show);
-        }
+    // Update the show with the files we found.
+    for file in files {
+        show.update(file)?;
+    }
+
+    // The show should be valid now. Time for a callback.
+    if show.is_valid() {
+        callback(show);
     }
 
     // Recursively walk through all the directories.
     for dir in dirs {
-        walker(&dir, callback)?;
+        walker(dir, callback)?;
     }
 
     Ok(())
