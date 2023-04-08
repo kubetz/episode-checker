020c550b6cdaabfc1e2ec0e464017b0e6ce9bdeb
kubetz <kubetzzz@gmail.com>
2023-04-08 02:50:22 +0200

    feat: create prelude and define errors

---
+++ src/prelude.rs
@@ -0,0 +1,24 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("I/O error: {0}")]
+    IO(#[from] std::io::Error),
+
+    #[error("Error parsing input {0}")]
+    Parse(String),
+
+    #[error("Invalid file {0}")]
+    InvalidFile(std::path::PathBuf),
+
+    #[error("Cannot load show {0}")]
+    CannotLoad(String),
+
+    #[error("Wrong JSON data for {0}")]
+    WrongJSON(String),
+
+    #[error("Matching episode not found for S{1:0>2}E{2:0>2} of {0}")]
+    NotFound(String, u16, u8),
+}
